d93d8e805760edffd47191dfd1006ef844ddb9a1
Eugene Yehanovskiy <yehanovskiy@gmail.com>
2021-01-10 22:16:49 +0200

    setup UIKit VC

---
+++ TinderClone/TinderClone/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Eugene Y on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
