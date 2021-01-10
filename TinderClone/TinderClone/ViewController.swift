01c53f9daad03b478fd9b33d3dd5dd8801c57027
Eugene Yehanovskiy <yehanovskiy@gmail.com>
2021-01-10 23:05:39 +0200

    snapKit. stackView for main screen

--- TinderClone/TinderClone/ViewController.swift
+++ TinderClone/TinderClone/ViewController.swift
@@ -6,11 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        view.backgroundColor = .white
+        
+        let topSubviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let view = UIView()
+            view.backgroundColor = color
+            return view
+        }
+        
+        let topView = UIStackView(arrangedSubviews: topSubviews)
+        topView.distribution = .fillEqually
+        topView.snp.makeConstraints { (make) in
+            make.height.equalTo(110)
+        }
+        
+        let middleView = UIView()
+        middleView.backgroundColor = .yellow
+        
+        let bottomView = UIView()
+        bottomView.backgroundColor = .red
+        bottomView.snp.makeConstraints { (make) in
+            make.height.equalTo(120)
+        }
+        
+        let mainStackView = UIStackView(arrangedSubviews: [topView, middleView, bottomView])
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fill
+        
+        view.addSubview(mainStackView)
+        
+        mainStackView.snp.makeConstraints { (make) in
+            make.top.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        
     }
 }
